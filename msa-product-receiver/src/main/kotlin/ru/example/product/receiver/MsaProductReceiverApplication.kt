77e5fb07ee4817f08201aac96982ed8dc2e5bed1
Vitaliy Sinabdeev <17719603+sinabdeev@users.noreply.github.com>
2026-04-20 22:49:38 +0400

    Init

---
+++ msa-product-receiver/src/main/kotlin/ru/example/product/receiver/MsaProductReceiverApplication.kt
@@ -0,0 +1,11 @@
+package ru.example.product.receiver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsaProductReceiverApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsaProductReceiverApplication>(*args)
+}
---
+++ msa-product-receiver/src/test/kotlin/ru/example/product/receiver/MsaProductReceiverApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.example.product.receiver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsaProductReceiverApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
